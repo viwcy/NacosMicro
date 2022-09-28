19dbca3ee99a43f1db4a866022c8b429afc2a9db
viwcy <fuqiangvn@com>
2022-09-28 11:39:19 +0800

    update constant

--- search/src/main/java/com/viwcy/search/vo/PageVO.java
+++ search/src/main/java/com/viwcy/search/vo/PageVO.java
@@ -1,9 +1,9 @@
 package com.viwcy.search.vo;
 
+import com.google.common.collect.Lists;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,7 +15,7 @@ public class PageVO<T> implements Serializable {
 
     private static final long serialVersionUID = -1538969892586525792L;
 
-    private List<T> result = Collections.synchronizedList(new ArrayList<>());
+    private List<T> result = Collections.synchronizedList(Lists.newArrayList());
 
     private long total;
 }
